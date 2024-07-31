13826ea5055034fd2940675021443495e01cb29e
Raihan <md.raihanbdrasel@gmail.com>
2024-07-31 22:47:46 +0600

    Less code, less bug

--- select.js
+++ select.js
@@ -18,29 +18,17 @@ function pathValidator(input, name) {
     return true;
 }
 const rootPath = path.join(__dirname + '/src');
-let selectedWebsite = null;
-let selectedCampaign = null;
-let selectedVariation = null;
+let selectedWebsite = null, selectedCampaign = null, selectedVariation = null;
 
 function selectWebsite() {
     return new Promise((resolve, reject) => {
         const websites = fs.readdirSync(rootPath);
         if (websites.length) websites.push(new inq.Separator());
         websites.push(PromptConstants.WEBSITE, PromptConstants.EXIT);
-        inq.prompt([{
-            type: 'list',
-            message: 'Select a website',
-            name: 'website',
-            choices: websites
-        },]).then((answers) => {
+        inq.prompt([{ type: 'list', message: 'Select a website', name: 'website', choices: websites }]).then((answers) => {
             if (answers.website === PromptConstants.EXIT) return reject();
             if (answers.website !== PromptConstants.WEBSITE) return resolve(selectedWebsite = answers.website);
-            inq.prompt([{
-                type: 'input',
-                message: 'Enter website name:',
-                name: 'website',
-                validate: () => pathValidator(answers.website, 'Website')
-            }]).then((answers) => {
+            inq.prompt([{ type: 'input', message: 'Enter website name:', name: 'website', validate: () => pathValidator(answers.website, 'Website') }]).then((answers) => {
                 selectedWebsite = answers.website;
                 fs.mkdirSync(path.join(rootPath, selectedWebsite));
                 resolve(selectedWebsite);
@@ -54,20 +42,10 @@ function selectCampaign() {
         const campaigns = fs.readdirSync(path.join(rootPath, selectedWebsite));
         if (campaigns.length) campaigns.push(new inq.Separator());
         campaigns.push(PromptConstants.CAMPAIGN, PromptConstants.EXIT);
-        inq.prompt([{
-            type: 'list',
-            message: 'Select a campaign',
-            name: 'campaign',
-            choices: campaigns
-        }]).then((answers) => {
+        inq.prompt([{ type: 'list', message: 'Select a campaign', name: 'campaign', choices: campaigns }]).then((answers) => {
             if (answers.campaign === PromptConstants.EXIT) return reject();
             if (answers.campaign !== PromptConstants.CAMPAIGN) return resolve(selectedCampaign = answers.campaign);
-            inq.prompt([{
-                type: 'input',
-                message: 'Enter campaign name:',
-                name: 'campaign',
-                validate: () => pathValidator(answers.campaign, 'Campaign')
-            }]).then((answers) => {
+            inq.prompt([{ type: 'input', message: 'Enter campaign name:', name: 'campaign', validate: () => pathValidator(answers.campaign, 'Campaign') }]).then((answers) => {
                 selectedCampaign = answers.campaign;
                 fs.mkdirSync(path.join(rootPath, selectedWebsite, selectedCampaign));
                 resolve(selectedCampaign);
@@ -81,20 +59,10 @@ function selectVariation() {
         const variations = fs.readdirSync(path.join(rootPath, selectedWebsite, selectedCampaign));
         if (variations.length) variations.push(new inq.Separator());
         variations.push(PromptConstants.VARIATION, PromptConstants.EXIT);
-        inq.prompt([{
-            type: 'list',
-            message: 'Select a variation',
-            name: 'variation',
-            choices: variations
-        }]).then((answers) => {
+        inq.prompt([{ type: 'list', message: 'Select a variation', name: 'variation', choices: variations }]).then((answers) => {
             if (answers.variation === PromptConstants.EXIT) return reject();
             if (answers.variation !== PromptConstants.VARIATION) return resolve(selectedVariation = answers.variation);
-            inq.prompt([{
-                type: 'input',
-                message: 'Enter variation name:',
-                name: 'variation',
-                validate: () => pathValidator(answers.variation, 'Variation')
-            }]).then((answers) => {
+            inq.prompt([{ type: 'input', message: 'Enter variation name:', name: 'variation', validate: () => pathValidator(answers.variation, 'Variation') }]).then((answers) => {
                 selectedVariation = answers.variation;
                 fs.mkdirSync(path.join(rootPath, selectedWebsite, selectedCampaign, selectedVariation));
                 fs.createWriteStream(path.join(rootPath, selectedWebsite, selectedCampaign, selectedVariation, 'index.js')).end();
@@ -110,8 +78,7 @@ selectWebsite().then(selectCampaign).then(selectVariation).then(() => {
         fs.readdirSync(path.join(rootPath, website)).forEach(campaign => {
             fs.readdirSync(path.join(rootPath, website, campaign)).forEach(variation => {
                 const isActiveNow = fs.existsSync(path.join(rootPath, website, campaign, variation, '.now'));
-                if (!isActiveNow) return;
-                fs.unlinkSync(path.join(rootPath, website, campaign, variation, '.now'));
+                if (isActiveNow) return fs.unlinkSync(path.join(rootPath, website, campaign, variation, '.now'));
             });
         });
     });
