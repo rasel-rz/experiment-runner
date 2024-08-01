c40f3110b7e7dbb7152be3b01849050e71501585
Raihan <md.raihanbdrasel@gmail.com>
2024-08-01 17:50:18 +0600

    Templates are supported now

--- select.js
+++ select.js
@@ -7,6 +7,8 @@ const PromptConstants = {
     WEBSITE: 'Create new website',
     CAMPAIGN: 'Create new campaign',
     VARIATION: 'Create new variation',
+    TEMPLATE: 'Create new template',
+    EMPTY: 'Empty',
     EXIT: 'Exit'
 }
 
@@ -18,15 +20,17 @@ function pathValidator(input, name) {
     return true;
 }
 const rootPath = path.join(__dirname + '/src');
+const templatesPath = path.join(__dirname, 'templates');
 let selectedWebsite = null, selectedCampaign = null, selectedVariation = null;
 
 function selectWebsite() {
     return new Promise((resolve, reject) => {
         const websites = fs.readdirSync(rootPath);
         if (websites.length) websites.push(new inq.Separator());
-        websites.push(PromptConstants.WEBSITE, PromptConstants.EXIT);
+        websites.push(PromptConstants.WEBSITE, PromptConstants.TEMPLATE, PromptConstants.EXIT);
         inq.prompt([{ type: 'list', message: 'Select a website', name: 'website', choices: websites }]).then((answers) => {
             if (answers.website === PromptConstants.EXIT) return reject();
+            if (answers.website === PromptConstants.TEMPLATE) return reject(createTemplate());
             if (answers.website !== PromptConstants.WEBSITE) return resolve(selectedWebsite = answers.website);
             inq.prompt([{ type: 'input', message: 'Enter website name:', name: 'website', validate: () => pathValidator(answers.website, 'Website') }]).then((answers) => {
                 selectedWebsite = answers.website;
@@ -64,10 +68,22 @@ function selectVariation() {
             if (answers.variation !== PromptConstants.VARIATION) return resolve(selectedVariation = answers.variation);
             inq.prompt([{ type: 'input', message: 'Enter variation name:', name: 'variation', validate: () => pathValidator(answers.variation, 'Variation') }]).then((answers) => {
                 selectedVariation = answers.variation;
-                fs.mkdirSync(path.join(rootPath, selectedWebsite, selectedCampaign, selectedVariation));
-                fs.createWriteStream(path.join(rootPath, selectedWebsite, selectedCampaign, selectedVariation, 'index.js')).end();
-                fs.createWriteStream(path.join(rootPath, selectedWebsite, selectedCampaign, selectedVariation, 'style.scss')).end();
-                return resolve(selectedVariation);
+                const templates = fs.readdirSync(templatesPath);
+                if (templates.length) templates.unshift(new inq.Separator());
+                templates.unshift(PromptConstants.EMPTY);
+                inq.prompt([{ type: 'list', message: 'Select a template', name: 'template', choices: templates }]).then((answers) => {
+                    const variationPath = path.join(rootPath, selectedWebsite, selectedCampaign, selectedVariation);
+                    fs.mkdirSync(variationPath);
+                    if (answers.template === PromptConstants.EMPTY) {
+                        fs.createWriteStream(path.join(variationPath, 'index.js')).end();
+                        fs.createWriteStream(path.join(variationPath, 'style.scss')).end();
+                    } else {
+                        const templatePath = path.join(templatesPath, answers.template);
+                        fs.copyFileSync(path.join(templatePath, 'index.js'), path.join(variationPath, 'index.js'));
+                        fs.copyFileSync(path.join(templatePath, 'style.scss'), path.join(variationPath, 'style.scss'));
+                    }
+                    return resolve(selectedVariation);
+                }).catch(reject);
             }).catch(reject);
         }).catch(reject);
     });
@@ -85,3 +101,13 @@ selectWebsite().then(selectCampaign).then(selectVariation).then(() => {
     console.log('>>> Selected variation: ' + [selectedWebsite, selectedCampaign, selectedVariation].join(' > '));
     fs.createWriteStream(path.join(rootPath, selectedWebsite, selectedCampaign, selectedVariation, '.now')).end();
 }).catch(() => true);
+
+function createTemplate() {
+    inq.prompt([{ type: 'input', message: 'Enter template name:', name: 'template', validate: (input) => pathValidator(input, 'Template') }]).then((answers) => {
+        const templatePath = path.join(templatesPath, answers.template);
+        if (fs.existsSync(templatePath)) return console.log('Template already exists');
+        fs.mkdirSync(templatePath);
+        fs.createWriteStream(path.join(templatePath, 'index.js')).end();
+        fs.createWriteStream(path.join(templatePath, 'style.scss')).end();
+    }).catch((e) => console.trace(e));
+}
